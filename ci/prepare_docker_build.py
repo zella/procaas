80b8015a07af5918fdda5afe1e7813224d5eb0f7
zella <drumirage@gmail.com>
2019-01-15 00:12:05 +0300

    working on ci

---
+++ ci/prepare_docker_build.py
@@ -0,0 +1,12 @@
+import os
+import shutil
+
+path = 'target/scala-2.12/'
+
+files = list((file for file in os.listdir(path)
+         if os.path.isfile(os.path.join(path, file))))
+print(files)
+assert len(files) == 1
+assert files[0].endswith('.jar')
+jar = os.path.join(path, files[0])
+shutil.copy(jar, 'ci/pyaas.jar')
